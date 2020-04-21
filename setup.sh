9df4cff3ae2104698f7006f8c852289856ad2654
MALWARE <57804710+MALW4R3@users.noreply.github.com>
2020-04-21 16:59:27 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,11 @@ gunzip metasploit_5.0.65-1_all.deb.gz
 dpkg -i metasploit_5.0.65-1_all.deb
 apt -f install
 mv metasec /data/data/com.termux/files/usr/bin
-chmod +x metasec
+cd ..
+cd ..
+cd usr
+cd bin
+chmod +x *
 cd /$HOME/METASEC
 clear
 cat g.txt
