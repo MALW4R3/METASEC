7d21eadc228afac6fa279a400624bd6bc0ca9705
MALWARE <57804710+MALW4R3@users.noreply.github.com>
2020-05-05 18:07:41 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 pkg install figlet
-cat l.txt
+pkg install ruby
+gem install lolcat
+cat l.txt | lolcat
 curl -LO https://github.com/termux/termux-packages/files/3995119/metasploit_5.0.65-1_all.deb.gz
 gunzip metasploit_5.0.65-1_all.deb.gz
 dpkg -i metasploit_5.0.65-1_all.deb
