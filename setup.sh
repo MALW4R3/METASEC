2b5ceed972e7638d743d9e3b9305356ea1fbccb4
MALWARE <57804710+MALW4R3@users.noreply.github.com>
2020-05-21 12:53:14 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -29,6 +29,8 @@ curl -LO https://github.com/termux/termux-packages/files/3995119/metasploit_5.0.
 gunzip metasploit_5.0.65-1_all.deb.gz
 dpkg -i metasploit_5.0.65-1_all.deb
 apt -f install
+pkg install unstable-repo
+pkg install metasploit
 mv metasec /data/data/com.termux/files/usr/bin
 cd ..
 cd ..
